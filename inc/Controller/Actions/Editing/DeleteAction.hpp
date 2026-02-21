d059b4279835ec6588a09f15479d5808999efdec
Moritz Pirer <moritz.pirer@student.tugraz.at>
2026-02-21 21:39:21 +0100

    refactor: cleaned up DeletAction absorbtion

--- inc/Controller/Actions/Editing/DeleteAction.hpp
+++ inc/Controller/Actions/Editing/DeleteAction.hpp
@@ -19,6 +19,16 @@ private:
     Position m_start;
     Position m_end;
     Position m_cursor; //where to restore the cursor to
+
+    /// @brief other is directly before or directly after this delete section
+    bool isAdjacent(std::shared_ptr<DeleteAction> other) const;
+
+    /// @brief m_deleted_content has at least one non-empty line
+    bool hasContent() const;
+    
+    void absorbBeforeCurrent(std::shared_ptr<DeleteAction> other);
+    void absorbAfterCurrent(std::shared_ptr<DeleteAction> other);
+    
 public:
     DeleteAction(Position start, Position end, Position cursor);
     DeleteAction(const DeleteAction&) = default;
@@ -33,6 +43,7 @@ public:
     bool canAbsorb(const std::shared_ptr<Action>& action) const override;
 
     void absorb(const std::shared_ptr<Action>& action) override;
+
 };
 
 #endif //DELETE_ACTION_HPP
--- src/Controller/Actions/Editing/DeleteAction.cpp
+++ src/Controller/Actions/Editing/DeleteAction.cpp
@@ -25,75 +25,87 @@ void DeleteAction::undo(EditorState& state) {
     state.moveCursorTo(m_cursor);
 }
 
+bool DeleteAction::isAdjacent(std::shared_ptr<DeleteAction> other) const {
+    if (this->m_start.row != other->m_start.row) {
+        return false;
+    }
+
+    return (this->m_start.column - 1 == other->m_end.column) 
+        || (other->m_start.column - 1 == this->m_end.column);
+}
+
+bool DeleteAction::hasContent() const {
+    for (const std::string& paragraph : m_deleted_content) {
+        if (!paragraph.empty()) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 bool DeleteAction::canAbsorb(const std::shared_ptr<Action>& action) const {
     auto delete_action = std::dynamic_pointer_cast<DeleteAction>(action);
 
+    // not a delete action
     if (delete_action == nullptr) {
-        return false; // not a delete action
+        return false; 
     }
-    if (this->m_start.row != delete_action->m_start.row) {
+
+    if (!isAdjacent(delete_action)) {
         return false;
     }
 
-    bool is_adjacent = (this->m_start.column - 1 == delete_action->m_end.column) 
-        || (this->m_end.column == delete_action->m_start.column - 1);
+    if (!delete_action->hasContent()) {
+        return false; 
+    }
+
+    return !std::isspace(delete_action->m_deleted_content.back().back());
+}
+
+void DeleteAction::absorbBeforeCurrent(std::shared_ptr<DeleteAction> other) {
+    this->m_start = other->m_start;
     
-    if (!is_adjacent) {
-        return false;
+    if (other->m_deleted_content.empty()) {
+        return;
     }
 
-    // TODO: still merging over space
-    if (delete_action->m_deleted_content.empty() || delete_action->m_deleted_content.back().empty()) {
-        return false; // don't merge empty action
+    std::string overlap = other->m_deleted_content.back() + this->m_deleted_content.front();
+    this->m_deleted_content.front() = overlap;
+
+    for (int i = static_cast<int>(other->m_deleted_content.size()) - 2; i >= 0; i--) {
+        this->m_deleted_content.insert(this->m_deleted_content.begin(), other->m_deleted_content.at(i));
+    }
+}
+
+void DeleteAction::absorbAfterCurrent(std::shared_ptr<DeleteAction> other) {
+    this->m_cursor = other->m_cursor;
+
+    if (other->m_deleted_content.empty()) {
+        return;
     }
 
-    return (delete_action->m_deleted_content.back().back() != ' ');
+    // account for text moving over
+    this->m_end.column = this->m_start.column + 
+        (this->m_deleted_content.back().length() + other->m_deleted_content.front().length()) - 1;
+
+    this->m_deleted_content.back().append(other->m_deleted_content.front());
+
+    // Add any subsequent lines from the other action
+    for (size_t i = 1; i < other->m_deleted_content.size(); ++i) {
+        this->m_deleted_content.push_back(other->m_deleted_content.at(i));
+    }
 }
 
 void DeleteAction::absorb(const std::shared_ptr<Action>& action) {
     auto other = std::static_pointer_cast<DeleteAction>(action);
 
-    // 1. Update the cursor to the latest position
-
-    // 2. Determine merge direction
-    // If other ends where this starts, it's a Backspace (Delete Left)
     bool is_backspace = (other->m_end.column + 1 == this->m_start.column);
+
     if (is_backspace) {
-        // PREPEND the text: [Other Text][This Text]
-        this->m_start = other->m_start;
-        
-        if (other->m_deleted_content.empty()) {
-            return;
-        }
-
-        // Merge the overlapping line (other's last line + our first line)
-        std::string overlap = other->m_deleted_content.back() + this->m_deleted_content.front();
-        this->m_deleted_content.front() = overlap;
-
-        // If the other action had multiple lines, insert them at the beginning
-        for (int i = static_cast<int>(other->m_deleted_content.size()) - 2; i >= 0; --i) {
-            this->m_deleted_content.insert(this->m_deleted_content.begin(), other->m_deleted_content.at(i));
-        }
+        absorbBeforeCurrent(other);
     }
     else {
-        // APPEND the text: [This Text][Other Text] (Forward Delete)
-
-        this->m_cursor = other->m_cursor;
-
-        // account for text moving over
-        this->m_end.column = this->m_start.column + 
-            (this->m_deleted_content.back().length() + 
-                other->m_deleted_content.front().length()) - 1;
-
-        if (other->m_deleted_content.empty()) {
-            return;
-
-        }
-        this->m_deleted_content.back().append(other->m_deleted_content.front());
-
-        // Add any subsequent lines from the other action
-        for (size_t i = 1; i < other->m_deleted_content.size(); ++i) {
-            this->m_deleted_content.push_back(other->m_deleted_content.at(i));
-        }
+        absorbAfterCurrent(other);
     }
 }
