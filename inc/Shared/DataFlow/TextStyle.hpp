eafc248281e4ec7014068f6e7dd1ac1fe8158daf
Moritz Pirer <moritz.pirer@student.tugraz.at>
2026-03-10 09:00:51 +0100

    add this file too

---
+++ inc/Shared/DataFlow/TextStyle.hpp
@@ -0,0 +1,30 @@
+///
+/// @file: TextStyle.hpp
+/// @description: description
+///
+/// @date: 2026-03-10
+/// @author: Moritz Pirer
+///
+
+#ifndef TEXT_STYLE_HPP
+#define TEXT_STYLE_HPP
+
+struct TextStyle {
+    bool is_bold;
+    bool is_italic;
+
+    TextStyle(bool bold, bool italic):
+        is_bold{bold},
+        is_italic{italic}
+        {}
+    
+    static TextStyle makeNormal() { return TextStyle{false, false}; }
+
+    static TextStyle makeBold() { return TextStyle{true, false}; }
+
+    static TextStyle makeItalic() { return TextStyle{false, true}; }
+    
+    static TextStyle makeBoldItalic() { return TextStyle{true, true}; }
+};
+
+#endif //TEXT_STYLE_HPP
